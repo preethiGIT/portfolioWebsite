af8dd9ba63f3b6e530b60793fc72732a4f18918f
Preethi <preethivijayapuri@gmail.com>
2020-08-14 16:00:58 -0500

    Updates

---
+++ precache-manifest.2476fe2a576c5f20ff9317eb8a602202.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "366b11823f427c17ad47d42a392861f0",
+    "url": "/portfolioWebsite/index.html"
+  },
+  {
+    "revision": "49d68240ddfa54c4795e",
+    "url": "/portfolioWebsite/static/css/main.e6b15e6f.chunk.css"
+  },
+  {
+    "revision": "2b2ede8e0636628624ca",
+    "url": "/portfolioWebsite/static/js/2.d85d5408.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/portfolioWebsite/static/js/2.d85d5408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49d68240ddfa54c4795e",
+    "url": "/portfolioWebsite/static/js/main.2143fab8.chunk.js"
+  },
+  {
+    "revision": "602c2d784656ae5dd3c7",
+    "url": "/portfolioWebsite/static/js/runtime-main.b915a624.js"
+  },
+  {
+    "revision": "09f5c6ae0d73877c4fc2595170735ff9",
+    "url": "/portfolioWebsite/static/media/github-mark.09f5c6ae.png"
+  },
+  {
+    "revision": "38213ef27a2203c9a7f0b50a34764bc4",
+    "url": "/portfolioWebsite/static/media/myImg.38213ef2.png"
+  },
+  {
+    "revision": "94ba72a52be08b600c7e3f01fe9dd267",
+    "url": "/portfolioWebsite/static/media/sky.94ba72a5.jpeg"
+  }
+]);
